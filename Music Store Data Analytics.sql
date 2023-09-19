335daca08d570cf3393a87e3c1387cfa6edd243b
PeddiKoushik <140744832+PeddiKoushik@users.noreply.github.com>
2023-09-20 01:55:23 +0530

    Update Music Store Data Analytics.sql

--- Music Store Data Analytics.sql
+++ Music Store Data Analytics.sql
@@ -1,4 +1,33 @@
-/* Q8: Find how much amount spent by each customers on artists? wrtie a
+/* Q1: Return all the track names that have a song length longer than the average song leght.
+return the name and milliseconds for each track. Order by the song length with lingest songs
+listed firs*/
+SELECT name, milliseconds
+FROM track
+WHERE milliseconds > (
+    SELECT AVG(milliseconds)  AS avg_track_length
+FROM track)
+ORDER BY milliseconds DESC
+
+
+
+
+/* Q2: Let's invite the asrtists who have written the most rock music in
+our dataset. Write a query that returns the Artist name and total
+trackk count of the top 10 bands*/
+SELECT artist.artist_id, artist.name, COUNT(artist.artist_id) AS number_of_songs
+FROM track
+JOIN album ON album.album_id =track.album_id
+JOIN artist ON artist.artist_id = album.artist_id
+JOIN genre ON genre.genre_id = track.genre_id
+WHERE genre.name LIKE'Rock'
+GROUP BY artist.artist_id
+ORDER BY number_of_songs DESC
+LIMIT 10
+
+
+
+  
+/* Q3: Find how much amount spent by each customers on artists? wrtie a
 query to return customer name, artist name and total spent*/
 WITH best_selling_artist AS(
 SELECT artist.artist_id AS artist_id, artist.name AS artist_name,
